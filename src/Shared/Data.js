09a50d1ba201ba874d905e5c663502f4e570a93d
TomcyT <tomcy48@gmail.com>
2022-07-10 15:58:29 +0530

    fixed works-cards for better UI

--- src/Shared/Data.js
+++ src/Shared/Data.js
@@ -2,13 +2,16 @@ export const Categories = [
   {
     id: 1,
     title: "ui ux design",
+    desc: "A few of my user experience designs completed during my Third Year",
   },
   {
     id: 2,
     title: "dev Projects",
+    desc: "Development projects I worked on during my Academic year",
   },
   {
     id: 3,
     title: "graphic design",
+    desc: "I started learning graphic designing during the start of my second year in college, I'm having 2 years of experience in graphic designing and here are few of my works",
   },
 ];
--- src/components/WorksCategory/CategoryItem.js
+++ src/components/WorksCategory/CategoryItem.js
@@ -5,7 +5,11 @@ import "./CategoryItem.scss";
 export const CategoryItem = ({ category }) => {
   return (
     <div className="category-item__container">
-      <h4>{category.title}</h4>
+      <div className="category-item__header">
+        <h4>{category.title}</h4>
+        <p>{category.desc}</p>
+      </div>
+
       <button>view</button>
     </div>
   );
