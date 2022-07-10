a5c2a0e2cc94f34bb56fa97db0a4f583ee13a5d3
TomcyT <tomcy48@gmail.com>
2022-07-10 22:48:42 +0530

    fixed navbar padding and added routing to navbar logo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,30 +1,35 @@
 import React from "react";
 import "./navbar.styles.scss";
-import { Link } from "react-scroll";
+import { Link as LinkR } from "react-router-dom";
+import { Link as LinkS } from "react-scroll";
 import TDLogo from "../../assets/Logo.png";
 import TDMobile from "../../assets/MobileLogo.png";
 const Navbar = () => {
   return (
-    <div className="navbar">
+    <div className="navbar" id="navbar">
       <div className="navbar__container">
         <div className="navbar__Logo">
-          <Link className="navbar__logo-link" to="home">
+          <LinkR className="navbar__logo-link" to="/">
             <img src={TDLogo} alt="Logo" />
             <div className="mobileLogo">
               <img src={TDMobile} alt="Logo" />
             </div>
-          </Link>
+          </LinkR>
         </div>
         <div className="navbar__Links">
-          <Link
+          <LinkS
             /* class="active" */ to="works-section"
             smooth={true}
             duration="1000"
           >
             Works
-          </Link>
-          <Link to="about">About</Link>
-          <Link to="contact">Contact</Link>
+          </LinkS>
+          <LinkS to="about" smooth={true} duration="1000">
+            About
+          </LinkS>
+          <LinkS to="contact" smooth={true} duration="1000">
+            Contact
+          </LinkS>
         </div>
       </div>
     </div>
