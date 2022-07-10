c3cd8c471ec1cb2328b8ceab906337dc8e70037f
TomcyT <tomcy48@gmail.com>
2022-07-10 22:50:57 +0530

    added UI UX component

---
+++ src/components/Routes/UiUX/UiUx.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const UiUx = () => {
+  return (
+    <div
+      className="ui-ux"
+      style={{
+        marginTop: "4rem",
+      }}
+    >
+      <div className="ui-ux__container" s>
+        <marquee behavior="" direction="">
+          This Section is under development
+        </marquee>
+      </div>
+    </div>
+  );
+};
+
+export default UiUx;
