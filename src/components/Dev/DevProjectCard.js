084a737b2b844b06cdb0cfffa8bf9a14819608a3
TomcyT <tomcy48@gmail.com>
2022-07-21 19:20:04 +0530

    added Development Projects section

--- src/components/Dev/DevProjectCard.js
+++ src/components/Dev/DevProjectCard.js
@@ -7,22 +7,22 @@ const DevProjectCard = ({ development }) => {
   return (
     <div className="dev-project-card">
       <div className="dev-project-card__container">
-        <div className="dev-project-card__image">
-          {" "}
-          <img src={image} alt={title} />
-        </div>
-
-        <div className="dev-project__right">
+        <div className="dev-project__details">
+          <div className="dev-project-card__image">
+            <img src={image} alt={title} />
+          </div>
           <div className="dev-project-card__title">
             <h3>{title}</h3>
           </div>
-          <div className="dev-project-card__description">{desc}</div>
-          <div className="dev-project-card__stack">{stack}</div>
-          <div className="dev-project-card__button">
-            <a href={link} target="_blank" rel="noreferrer">
-              <button>Check out !</button>
-            </a>
+          <div className="dev-project-card__description">
+            <p>{desc}</p>
           </div>
+          <div className="dev-project-card__stack">{stack}</div>
+        </div>
+        <div className="dev-project-card__button">
+          <a href={link} target="_blank" rel="noreferrer">
+            <button>Check out !</button>
+          </a>
         </div>
       </div>
     </div>
