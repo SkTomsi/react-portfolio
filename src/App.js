42d8b0eb1b700602e127987b5fe2f0f2c0419aa7
TomcyT <tomcy48@gmail.com>
2022-07-29 18:19:05 +0530

    added loader and scroll to top functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
+/* MODULES */
+import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import ScrollToTop from "react-scroll-to-top";
+
+/* STYLING */
 import "./App.css";
 
-import { Routes, Route } from "react-router-dom";
+/* COMPONENTS */
 import Navbar from "./components/Navbar";
 import Home from "./Routes/Home/Home";
 import UiUx from "./Routes/UiUX/UiUx";
 import About from "./components/About";
 
+/* LOWER ORDER */
+import CustomLoader from "./components/CustomLoader";
+
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/ui-ux" element={<UiUx />} />
-      </Routes>
+      {loading ? (
+        <CustomLoader />
+      ) : (
+        <div className="app__container">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/ui-ux" element={<UiUx />} />
+          </Routes>
+          <ScrollToTop smooth color="purple" />
+        </div>
+      )}
     </div>
   );
 }
