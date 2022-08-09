9d2ed4c96bd7abc23d5af419edae950206168513
TomcyT <tomcy48@gmail.com>
2022-08-09 21:56:24 +0530

    [refactor]: removed works category section for better user experience

--- src/Shared/Data.js
+++ src/Shared/Data.js
@@ -1,24 +1,3 @@
-export const Categories = [
-  {
-    id: 1,
-    title: "ui ux designs",
-    desc: "A few of my user experience designs completed during my Third Year",
-    scrollComponent: "uiux",
-  },
-  {
-    id: 2,
-    title: "dev projects",
-    desc: "Development projects I worked on during my Academic year",
-    scrollComponent: "dev-project",
-  },
-  {
-    id: 3,
-    title: "graphic designs",
-    desc: "2 years of experience in graphic designing and here are few of my works",
-    scrollComponent: "gfx",
-  },
-];
-
 export const uxProjects = [
   {
     id: 1,
