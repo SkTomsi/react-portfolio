366a028550830c1e371d68306d312cc1e2c25570
TomcyT <tomcy48@gmail.com>
2023-05-03 12:46:03 +0530

    migrate to nextjs and refactor css

---
+++ src/components/heroSection.js
@@ -0,0 +1,42 @@
+import React from "react";
+/* import TypeRoles from "./TypeRoles"; */
+import styles from "@/styles/HeroSection.module.scss";
+import { Link } from "react-scroll";
+
+const HeroSection = () => {
+  return (
+    <div className={styles.heroSection} id="hero-section">
+      <div className={styles.heroContainer}>
+        <div className={styles.heroIntro}>
+          <p>Hi! my name is Tomcy, I love</p>
+          <h1>
+            building <span className={styles.heroColor}>products</span> and{" "}
+            <span className={styles.heroColor2}>experiences</span>.
+          </h1>
+          <div className={styles.heroShortIntro}>
+            <p>
+              Final Year Computer Engineering student from Mumbai, working as a
+              freelance User Experience Designer since a year and currently
+              building cool stuff with ReactJS and React Native
+            </p>
+          </div>
+        </div>
+        <div className={styles.heroCta}>
+          <Link to="ui-ux" smooth={true} className={styles.heroCtaButtonLink}>
+            <button>Check out my works!</button>
+          </Link>
+          <a
+            href="https://smallify.vercel.app/resume"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.resume}
+          >
+            View my Resume
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ src/components/navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "@/styles/Navbar.module.scss";
+import Link from "next/link";
+import TDLogo from "../../public/images/Logo.png";
+import TDMobile from "../../public/images/MobileLogo.png";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar} id="navbar">
+      <div className={styles.navbarContainer}>
+        <div className={styles.navbarLogo}>
+          <Link className={styles.navbarLogoLink} href="/">
+            <Image src={TDLogo} alt="Logo" height={64} />
+
+            <div className={styles.mobileLogo}>
+              <Image src={TDMobile} alt="Logo" height={48} />
+            </div>
+          </Link>
+        </div>
+        <div className={styles.navbarLinks}>
+          <Link href="/">Home</Link>
+          <Link href="about">About</Link>
+          <Link href="/">Contact</Link>
+          <a
+            href="https://smallify.vercel.app/resume"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.resume}
+          >
+            Resume
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
